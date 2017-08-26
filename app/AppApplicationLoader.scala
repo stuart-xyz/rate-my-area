e67bdf454092f5094674eedec60aab3caf287d5e
Stuart Phillips <stuart.p@livingmap.com>
2017-08-26 22:54:52 +0100

    refactor: clean up application loader

--- app/AppApplicationLoader.scala
+++ app/AppApplicationLoader.scala
@@ -3,13 +3,13 @@ import com.softwaremill.macwire._
 import modules.{ControllerModule, DatabaseModule}
 import play.api.ApplicationLoader.Context
 import play.api._
+import play.api.cache.SyncCacheApi
 import play.api.cache.ehcache.EhCacheComponents
 import play.api.http.HttpErrorHandler
 import play.api.mvc.{AnyContent, BodyParser}
 import play.api.routing.Router
 import play.filters.HttpFiltersComponents
 import router.Routes
-import services.AuthService
 
 import scala.concurrent.ExecutionContext
 
@@ -32,19 +32,18 @@ class AppComponents(context: Context) extends BuiltInComponentsFromContext(conte
     configurator.configure(context.environment)
   }
 
-  // this will actually run the database migrations on startup
+  // this will run the database migrations on startup
   applicationEvolutions
 
   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
 
   lazy val thisHttpErrorHandler: HttpErrorHandler = httpErrorHandler
   lazy val bodyParser: BodyParser[AnyContent] = playBodyParsers.default
+  lazy val defaultSyncCacheApi: SyncCacheApi = defaultCacheApi.sync
 
   lazy val router: Router = {
     implicit val prefix: String = "/"
     wire[Routes]
   }
-
-  lazy val authService = new AuthService(defaultCacheApi.sync, databaseService)
 
 }
--- app/modules/ControllerModule.scala
+++ app/modules/ControllerModule.scala
@@ -1,6 +1,7 @@
 package modules
 
 import controllers.{AuthController, HomeController}
+import play.api.cache.SyncCacheApi
 import play.api.mvc.{AnyContent, BodyParser, ControllerComponents}
 import services.{AuthService, DatabaseService, UserAuthAction}
 
@@ -13,9 +14,11 @@ trait ControllerModule {
   // dependencies
   implicit def ec: ExecutionContext
   def databaseService: DatabaseService
-  def authService: AuthService
   def controllerComponents: ControllerComponents
   def bodyParser: BodyParser[AnyContent]
+  def defaultSyncCacheApi: SyncCacheApi
+
+  lazy val authService = new AuthService(defaultSyncCacheApi, databaseService)
 
   lazy val homeController: HomeController = wire[HomeController]
   lazy val authController: AuthController = wire[AuthController]
