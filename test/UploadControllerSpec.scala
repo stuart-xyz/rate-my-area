b45d5617d6e8b1d009264c8e10f28131e9a05359
Stuart Phillips <stuart.p@livingmap.com>
2017-09-24 11:45:48 +0100

    test: upload controller

--- test/UploadControllerSpec.scala
+++ test/UploadControllerSpec.scala
@@ -1,11 +1,16 @@
 import org.scalatestplus.play.{BaseOneAppPerTest, PlaySpec}
-import play.api.libs.json.{JsObject, Json}
+import play.api.libs.json.Json
 import play.api.test.Helpers._
-
-import scala.concurrent.ExecutionContext.Implicits.global
 
 class UploadControllerSpec extends PlaySpec with AuthenticatedUser with TestHelpers with BaseOneAppPerTest with AppApplicationFactory {
 
+  "POST /upload" should {
 
+    "return HTTP 401 unauthorised without authorisation cookie" in {
+      val result = makeSimpleRequest("upload", authCookieOption = None, jsonBody = Some(Json.obj()), POST)
+      validateResult(result, UNAUTHORIZED, "error")
+    }
+
+  }
 
 }
