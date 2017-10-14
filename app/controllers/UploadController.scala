a5249372fd490e49c589cba7efde3ec4bd825ba4
Stuart Phillips <stuart.p@livingmap.com>
2017-10-14 22:43:26 +0100

    refactor: use separate thread pool for image uploads

--- app/controllers/UploadController.scala
+++ app/controllers/UploadController.scala
@@ -2,21 +2,27 @@ package controllers
 
 import java.util.UUID
 
+import akka.actor.ActorSystem
 import play.api.libs.json.Json
 import play.api.mvc._
 import services.{S3Service, UserAuthAction}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class UploadController(cc: ControllerComponents, userAuthAction: UserAuthAction, s3Service: S3Service)(implicit ec: ExecutionContext)
   extends AbstractController(cc) {
 
-  def upload: Action[AnyContent] = userAuthAction { implicit request =>
-    val urls = request.body.asMultipartFormData.get.files.map(file => {
-      val filename = UUID.randomUUID()
-      s3Service.upload(file.ref.toFile, filename.toString, request.user.id)
-    })
-    Ok(Json.obj("message" -> "Files uploaded", "urls" -> urls))
+  val akkaSystem = ActorSystem("ratemyarea")
+  val executionContext: ExecutionContext = akkaSystem.dispatchers.lookup("upload-context")
+
+  def upload: Action[AnyContent] = userAuthAction.async { implicit request =>
+    Future {
+      val urls = request.body.asMultipartFormData.get.files.map(file => {
+        val filename = UUID.randomUUID()
+        s3Service.upload(file.ref.toFile, filename.toString, request.user.id)
+      })
+      Ok(Json.obj("message" -> "Files uploaded", "urls" -> urls))
+    }(executionContext)
   }
 
 }
