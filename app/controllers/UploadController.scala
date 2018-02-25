47f5c2a5c49788bae605f99529e457add64da888
Stuart Phillips <stuart.p@livingmap.com>
2018-02-25 23:37:43 +0000

    refactor: adjust thread pool configuration

--- app/controllers/UploadController.scala
+++ app/controllers/UploadController.scala
@@ -2,7 +2,6 @@ package controllers
 
 import java.util.UUID
 
-import akka.actor.ActorSystem
 import play.api.libs.json.Json
 import play.api.mvc._
 import services.S3Service
@@ -12,9 +11,6 @@ import scala.concurrent.{ExecutionContext, Future}
 class UploadController(cc: ControllerComponents, userAuthAction: UserAuthAction, s3Service: S3Service)(implicit ec: ExecutionContext)
   extends AbstractController(cc) {
 
-  val akkaSystem = ActorSystem("ratemyarea")
-  val executionContext: ExecutionContext = akkaSystem.dispatchers.lookup("upload-context")
-
   def upload: Action[AnyContent] = userAuthAction.async { implicit request =>
     Future {
       val urls = request.body.asMultipartFormData.get.files.map(file => {
@@ -22,7 +18,7 @@ class UploadController(cc: ControllerComponents, userAuthAction: UserAuthAction,
         s3Service.upload(file.ref.toFile, filename.toString, request.user.id)
       })
       Ok(Json.obj("message" -> "Files uploaded", "urls" -> urls))
-    }(executionContext)
+    }
   }
 
 }
